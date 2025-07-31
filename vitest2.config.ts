d8e16056ef30372ed3d0b1559606cb6b6dc8e0a6
Michael Wu <mw10013@gmail.com>
2025-07-31 19:38:36 -0400

    Add Vitest configuration and new test scripts for enhanced testing capabilities

---
+++ vitest2.config.ts
@@ -0,0 +1,22 @@
+import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+
+import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineWorkersConfig({
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  test: {
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: "./wrangler.jsonc" },
+      },
+    },
+  },
+});
